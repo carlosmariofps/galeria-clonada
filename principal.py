4e50a5d1eb6eef2822184c2f1674b77e98dc9031
Alumno <alumno@cepibase.int>
2022-07-19 13:02:34 +0200

    agrego una nueva función

--- principal.py
+++ principal.py
@@ -2,6 +2,12 @@ from flask import Flask, request, render_template
 from werkzeug.utils import secure_filename
 import os
 
+
+def comprueba_fondo():
+	pass
+
+DOCUMENTOS = ["doc", "docx"]
+
 EXTENSIONES = ["png", "jpg", "jpeg"]
 app = Flask(__name__)
 app.config["UPLOAD_FOLDER"] = './static/archivos'
