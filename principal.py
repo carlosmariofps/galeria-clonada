7810da844fdb2d3cff785328de68958a6bbebf26
Alumno <alumno@cepibase.int>
2022-07-19 13:00:16 +0200

    he añadido dos funciones

--- principal.py
+++ principal.py
@@ -2,6 +2,13 @@ from flask import Flask, request, render_template
 from werkzeug.utils import secure_filename
 import os
 
+
+def usuario():
+	pass
+
+def password():
+	pass
+
 EXTENSIONES = ["png", "jpg", "jpeg"]
 app = Flask(__name__)
 app.config["UPLOAD_FOLDER"] = './static/archivos'
